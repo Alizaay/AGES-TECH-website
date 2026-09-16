b9ccb65802aecf648d8ca0b48c5ea99e7d6d8f18
agestech7 <234759827+agestech7@users.noreply.github.com>
2026-09-15 20:44:47 -0400

    Update homepage statistics

--- src/data/home/statistics.js
+++ src/data/home/statistics.js
@@ -1,31 +1,31 @@
 export const statistics = [
   {
     id: 1,
-    value: '25+',
+    value: '5+',
     label: 'Industries Served',
     icon: 'industries',
   },
   {
     id: 2,
-    value: '120+',
+    value: '12+',
     label: 'Projects Delivered',
     icon: 'projects',
   },
   {
     id: 3,
-    value: '30+',
+    value: '3+',
     label: 'Global Partners & Integrations',
     icon: 'partners',
   },
   {
     id: 4,
-    value: '95%',
+    value: '98%',
     label: 'Client Satisfaction Rate',
     icon: 'satisfaction',
   },
   {
     id: 5,
-    value: '10+',
+    value: '25+',
     label: 'Years of Combined Experience',
     icon: 'experience',
   },
